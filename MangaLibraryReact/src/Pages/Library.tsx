77147b31477a40dc7f1b870de358bf0a625af74c
miki2017milan <milanmilenkovic20@gmail.com>
2025-09-18 21:41:32 +0200

    Chaned /libary wlcome message

--- MangaLibraryReact/src/Pages/Library.tsx
+++ MangaLibraryReact/src/Pages/Library.tsx
@@ -1,7 +1,7 @@
 export default function Library() {
     return (
         <>
-            <h1>Welcome to the Manga Library!</h1>
+            <h1>Welcome to your Manga Library!</h1>
             <br />
             <p>Here you can manage your own Collection.</p>
         </>
