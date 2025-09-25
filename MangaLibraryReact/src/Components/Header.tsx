2037a491144253324a0776a501f50c1ea8899c6a
miki2017milan <milanmilenkovic20@gmail.com>
2025-09-25 12:35:14 +0200

    started with responsive design

--- MangaLibraryReact/src/Components/Header.tsx
+++ MangaLibraryReact/src/Components/Header.tsx
@@ -3,15 +3,17 @@ import HeaderButtons from "./HeaderButtons";
 
 export default function Header() {
     return (
-        <div className="header">
-            <div className="header-content">
-                <Link to="/">
-                    <div className="logo">
-                        <img src="logo.webp" />
-                        <h2>Manga Library</h2>
-                    </div>
-                </Link>
-                <div className="full-w">
+        <nav className="header">
+            <ul className="header-content">
+                <li>
+                    <Link to="/">
+                        <div className="logo">
+                            <img src="logo.webp" />
+                            <h2>Manga Library</h2>
+                        </div>
+                    </Link>
+                </li>
+                <li>
                     <div className="header-nav">
                         <Link className="nav-link" to="/search">
                             Search
@@ -20,12 +22,13 @@ export default function Header() {
                             My Library
                         </Link>
                     </div>
-                </div>
-
-                <div className="header-buttons">
-                    <HeaderButtons></HeaderButtons>
-                </div>
-            </div>
-        </div>
+                </li>
+                <li>
+                    <div className="header-buttons">
+                        <HeaderButtons></HeaderButtons>
+                    </div>
+                </li>
+            </ul>
+        </nav>
     );
 }
