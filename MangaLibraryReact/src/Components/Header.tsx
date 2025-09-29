fe65ceb122d6f56c1a10321b6e8904907d58fdc8
Milan Milenkovic <milanmilenkovic20@gmail.com>
2025-09-29 03:28:11 +0200

    Left navbar on smaller devices is now fully functional

--- MangaLibraryReact/src/Components/Header.tsx
+++ MangaLibraryReact/src/Components/Header.tsx
@@ -1,15 +1,21 @@
 import { Link } from "react-router-dom";
 import HeaderButtons from "./HeaderButtons";
 
-export default function Header() {
+type data = {
+    show: boolean
+    toggleShowHeader: Function
+}
+
+export default function Header( { show, toggleShowHeader } : data ) {
     return (
-        <nav className="header">
+        <nav className={show ? "header show" : "header"}>
             <ul className="header-content">
                 <li>
                     <Link to="/">
                         <div className="logo">
                             <img src="logo.webp" />
                             <h2>Manga Library</h2>
+                            <img className="closeButton" src="close-button.webp" onClick={() => toggleShowHeader()} />
                         </div>
                     </Link>
                 </li>
--- MangaLibraryReact/src/Layouts/MainLayout.tsx
+++ MangaLibraryReact/src/Layouts/MainLayout.tsx
@@ -1,12 +1,21 @@
 import Header from "../Components/Header";
 
+import { useState } from 'react';
 import { Outlet } from "react-router-dom";
 
 function MainLayout() {
+    const [ showHeader, setShowHeader ] = useState(false);
+
+    const toggleShowHeader = () => {
+        setShowHeader(prev => !prev);
+    };
+
     return (
         <div className="main-layout">
-            <Header></Header>
+            <Header toggleShowHeader={toggleShowHeader} show={showHeader} ></Header>
+            <div className="overlay" onClick={() => toggleShowHeader()}></div>
             <div className="content">
+                <img className="menuButton" src="menu-button.webp" onClick={() => toggleShowHeader()}/>
                 <Outlet></Outlet>
             </div>
         </div>
