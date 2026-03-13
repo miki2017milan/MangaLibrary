98b06b517f089f3b3ed2d038e5cc3843930dc24f
Milan Milenkovic <milanmilenkovic20@gmail.com>
2026-03-13 16:30:33 +0100

    removed react from github

--- MangaLibraryReact/src/Pages/Library.tsx
+++
@@ -1,9 +0,0 @@
-export default function Library() {
-    return (
-        <>
-            <h1>Welcome to your Manga Library!</h1>
-            <br />
-            <p>Here you can manage your own Collection.</p>
-        </>
-    );
-}
--- MangaLibraryReact/src/Pages/NotFound.tsx
+++
@@ -1,9 +0,0 @@
-export default function NotFound() {
-    return (
-        <>
-            <h1>Error 404!</h1>
-            <h2>Page not found</h2>
-
-        </>
-    );
-}
